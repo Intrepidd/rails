27a4d8e61e664e7873de8c13d28fca42c4fc0ad5
Adrien Siami <adrien.siami@gmail.com>
2019-12-08 18:02:04 +0100

    Use safe_join instead of join + html_safe

--- actionview/lib/action_view/helpers/csrf_helper.rb
+++ actionview/lib/action_view/helpers/csrf_helper.rb
@@ -21,10 +21,10 @@ module ActionView
       #
       def csrf_meta_tags
         if defined?(protect_against_forgery?) && protect_against_forgery?
-          [
+          safe_join([
             tag("meta", name: "csrf-param", content: request_forgery_protection_token),
             tag("meta", name: "csrf-token", content: form_authenticity_token)
-          ].join("\n").html_safe
+          ], "\n")
         end
       end
 
